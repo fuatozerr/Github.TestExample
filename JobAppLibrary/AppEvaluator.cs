da0f3a7ee4ebc6153c19b03ccd9e2ffb55deb376
Fuat <fuatozerr23@gmail.com>
2022-07-16 13:22:11 +0300

    adding enum

---
+++ JobAppLibrary/AppEvaluator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobAppLibrary
+{
+    public class AppEvaluator
+    {
+        public ApplicationResult Evaluate()
+        {
+            return ApplicationResult.AutoAccepted;
+        }
+
+        public enum ApplicationResult
+        {
+            AutoRejected,
+            TransferredToHR,
+            TransferredToLead,
+            TransferredToCTO,
+            AutoAccepted
+        }
+    }
+}
