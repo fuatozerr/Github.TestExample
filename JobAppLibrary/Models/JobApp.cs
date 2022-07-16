ec9d1b813cfe8a24087bc6fc8cf629cc9e977cfd
Fuat <fuatozerr23@gmail.com>
2022-07-16 13:20:28 +0300

    adding classes

---
+++ JobAppLibrary/Models/JobApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobAppLibrary.Models
+{
+    public class JobApp
+    {
+        public Applicant Applicant { get; set; }
+        public int YearsOfExperince { get; set; }
+        public List<string> TechStackList { get; set; }
+
+    }
+}
